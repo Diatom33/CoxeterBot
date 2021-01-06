4278ba5fe80cbc836264ce66b0b4081c772aa75b
Violeta Hernández <eric.ivan.hdz@gmail.com>
2021-01-06 13:56:04 -0600

    Added edge labels and Graph class.

--- node.py
+++ node.py
@@ -4,35 +4,54 @@ class Node:
     def __init__(self, value):
         self.value = value
         self.neighbors = []
+        self.edgeLabels = []
         self.visited = False
     
     #Links two nodes together.
-    def linkTo(self, node):
+    def linkTo(self, node, label):
         self.neighbors.append(node)
+        self.edgeLabels.append(label)
         node.neighbors.append(self)
-
-    #Gets the connected components of a graph.
-    def components(self, graph):
-        components = []
-        
-        for node in graph:
-            if not node.visited:
-                components.append(node.component())
-                
-        return components
+        node.edgeLabels.append(label)
 
     #Gets the connected component of a node.
     def component(self):
-        Node._comp = []        
-        self._component()
+        Node.__comp = []        
+        self.__component()
         
         return Node._comp
         
     #Auxiliary function for component.
-    def _component(self):
+    def __component(self):
         self.visited = True
-        Node._comp.append(self)
+        Node.__comp.append(self)
         
         for node in self.neighbors:
             if not node.visited:
-                node._component()
+                node.__component()
+                
+class Graph:
+    #Class constructor.
+    def __init__(self, array):
+        self.array = array
+        self.idx = 0
+        return self
+        
+    #Iterator.
+    def __next__(self):
+        if self.idx < len(self.array):
+            x = self.array[self.idx]
+            self.idx += 1
+            return x
+            
+        raise StopIteration
+        
+    #Gets the connected components of a graph.
+    def components(self):
+        components = []
+        
+        for node in self:
+            if not node.visited:
+                components.append(node.component())
+                
+        return components
