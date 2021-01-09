9c9202873de87c522867d7ce89857dc83a24e2b5
Diatom33 <71231865+Diatom33@users.noreply.github.com>
2021-01-09 11:51:28 -0800

    Add files via upload

--- CDBot.py
+++ CDBot.py
@@ -5,54 +5,48 @@ import logging
 import datetime
 from PIL import Image, ImageDraw
 import os
+import re
+import node
 
 invite_link = "https://discord.com/api/oauth2/authorize?client_id=795909275880259604&permissions=34816&scope=bot"
 token = ""
 
-client = commands.Bot(command_prefix="&")
-
+client = commands.Bot(command_prefix=";")
 
 @client.event
 async def on_ready():
-    await client.change_presence(status=discord.Status.online, activity=discord.Activity(type=discord.ActivityType.watching, name="=help"))
+    await client.change_presence(status=discord.Status.online, activity=discord.Activity(type=discord.ActivityType.watching, name=";help"))
     a_logger.info("bot is ready")
 client.remove_command("help")
 
 a_logger = logging.getLogger()
 a_logger.setLevel(logging.INFO)
-
-output_file_handler = logging.FileHandler("Logs/log" +
-datetime.datetime.now().strftime("%Y-%m-%d %H-%M-%S") +
- ".txt")
+output_file_handler = logging.FileHandler("Logs/log" + 
+    datetime.datetime.now().strftime("%Y-%m-%d %H-%M") + ".txt")
 stdout_handler = logging.StreamHandler(sys.stdout)
-
 a_logger.addHandler(output_file_handler)
 a_logger.addHandler(stdout_handler)
-
 
 @client.command(pass_context=True)
 async def help(ctx):
 
     embed = discord.Embed(
+        title="CDBot Help",
         colour=discord.Colour.blue()
     )
 
-    embed.set_author(name="Help")
-    embed.add_field(name="`=help`",
-                    value="you said this", inline=False)
-    embed.add_field(name="`=cd [linearized diagram]`",
-                    value="Coxeter-Dynkin Diagram\nURL needs to make this still", inline=False)
-    
+    embed.set_author(name="by Cirro, Diatom, & URL")
+    embed.add_field(name="`;help`", value="you said this", inline=False)
+    embed.add_field(name="`;cd [linearized diagram]`",
+        value="Coxeter-Dynkin Diagram\nURL and/or Cirro needs to make this still", inline=False)
 
     await ctx.send(embed=embed)
     a_logger.info("helped")
-
 
 @client.command()
 async def ping(ctx):
     await ctx.send(f"pong {round(client.latency*1000)}ms")
     a_logger.info(f"pong {round(client.latency*1000)}ms")
-
 
 @client.command()
 async def invite(ctx):
@@ -60,12 +54,15 @@ async def invite(ctx):
     a_logger.info("invite sent")
 
 @client.command()
-async def cd(ctx, *, cd = "x"):
+async def cd(ctx, *, cd="default"):
     a_logger.info(f"cd: {cd}")
-    #Image making info here
-    img = Image.new('RGB', (60, 30), color = 'red')
-    #
-    img.save("temp.png")
+    if cd == "default":
+        temp = Image.new('RGB', (60, 30), color='white')
+    else:
+        # <make_image>
+        pass
+        # </make_image>
+    temp.save("temp.png")
     await ctx.send(file=discord.File("temp.png"))
     os.remove("temp.png")
 
